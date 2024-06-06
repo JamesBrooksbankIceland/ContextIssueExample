7366b0a1c1bd8f079dd5a1bd167b4a5b352d0a29
JamesBrooksbankIceland <150462478+JamesBrooksbankIceland@users.noreply.github.com>
2024-06-06 12:52:12 +0100

    Add project files.

---
+++ ContextIssueExample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ContextIssueExample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
